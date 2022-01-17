fb2ec1b5ce5b5a46ff6d5e0e5880354fea859151
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-17 11:01:07 -0600

    remove react reference

--- src/hooks/useColor.ts
+++ src/hooks/useColor.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Color, ColorObject } from '../types';
 import { initColor } from '../components/ColorPicker/helper';
